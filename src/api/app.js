450c029b413797a36f4e9d70b9077dce02b1e7e8
Anton <antonymousfeedback@gmail.com>
2021-06-09 23:33:47 +0200

    Remove favicon and public folder code

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,3 @@
-const path = require('path')
-const favicon = require('serve-favicon')
 const compress = require('compression')
 const helmet = require('helmet')
 const cors = require('cors')
@@ -31,9 +29,6 @@ app.use(cors())
 app.use(compress())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-// app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
-// Host the public folder
-// app.use('/', express.static(app.get('public')))
 
 // Set up Plugins and providers
 app.configure(express.rest())
