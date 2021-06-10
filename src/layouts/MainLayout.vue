98cba49ad87684ec7732326b1e14df7ec4b51d6d
anton <dasantonym@users.noreply.github.com>
2021-06-10 18:17:02 +0200

    Add navigation, authentication

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,21 +2,48 @@
   q-layout(view="lHh Lpr lFf")
     q-header(elevated)
       q-toolbar
-        q-btn(
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu")
+        q-btn-dropdown(icon="menu" flat)
+          q-list
+            q-item(:to="{ name: 'productions' }" clickable v-ripple v-close-popup
+              :active="$route.params.name === 'productions'"
+              active-class="text-accent")
+              q-item-section
+                q-item-label {{ $t('labels.productions') }}
+          q-item(:to="{ name: 'buckets' }" clickable v-ripple v-close-popup
+            :active="$route.params.name === 'buckets'"
+            active-class="text-accent")
+            q-item-section
+              q-item-label {{ $t('labels.buckets') }}
 
         q-toolbar-title LIGNA Server
+
+        template(v-if="isAuthenticated")
+          q-btn.q-mr-md(flat :label="user.name")
+          q-btn(flat :label="$t('buttons.logout')")
+        template(v-else)
+          q-btn(flat :label="$t('buttons.login')" :to="{ name: 'login' }" tag="a")
 
     q-page-container
       router-view
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  async created () {
+    try {
+      await this.$store.dispatch('auth/authenticate')
+    }
+    catch (err) {
+      console.error('Auth fail', err.message)
+    }
+  },
+  computed: {
+    ...mapGetters({
+      user: 'auth/user',
+      isAuthenticated: 'auth/isAuthenticated'
+    })
+  }
 }
 </script>
