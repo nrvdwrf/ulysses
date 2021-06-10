60ad7ad7b123e3a0ce9dcdd964f577a680dd72e0
anton <dasantonym@users.noreply.github.com>
2021-06-10 18:16:30 +0200

    Register new services

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,7 +1,11 @@
 const users = require('./users/users.service.js')
 const productions = require('./productions/productions.service.js')
+const assets = require('./assets/assets.service.js')
+const buckets = require('./buckets/buckets.service.js')
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users)
   app.configure(productions)
+  app.configure(assets)
+  app.configure(buckets)
 }
