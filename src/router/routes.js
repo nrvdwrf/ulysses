1af3886f4e4479e59a23b46f4303bc64307dcf25
anton <dasantonym@users.noreply.github.com>
2021-06-10 18:16:34 +0200

    Add routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,8 @@ const routes = [
     children: [
       { name: 'index', path: '', component: () => import('pages/Index.vue') },
       { name: 'login', path: 'login', component: () => import('pages/login.vue') },
+      { name: 'buckets', path: 'buckets', component: () => import('pages/buckets.vue') },
+      { name: 'buckets.edit', path: 'buckets/:id', component: () => import('pages/buckets_show.vue') },
       { name: 'productions', path: 'productions', component: () => import('pages/productions.vue') }
     ]
   },
