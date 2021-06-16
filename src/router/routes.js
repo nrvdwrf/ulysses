fea6cb1817209500f929f2427553b937a2d0e7de
anton <dasantonym@users.noreply.github.com>
2021-06-16 20:02:34 +0200

    Add production stub

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { name: 'index', path: '', component: () => import('pages/Index.vue') },
+      { name: 'content', path: '/p/:id', redirect: { name: 'index' } },
       { name: 'privacy', path: 'privacy', component: () => import('pages/privacy.vue') }
     ]
   },
